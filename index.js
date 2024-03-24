c8d6e943c183d7b6397e09353e69a7bf96f2179f
mikey <michaelmykie71@gmail.com>
2024-03-24 15:45:32 +0300

    loop

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, name) {
+    let emptyArrays =[];
+    for (let i= 0; i < array.length; i++) {
+      let emptyArray =`Thank you ${array[i]}${name}.`;
+      emptyArrays.push(emptyArray);
+      debugger;
+    }
+    return emptyArrays;
+  }
+  function countDown(num) {
+    while (num>=0) {
+      console.log(num--);
+    }
+  }
+  console.log(countDown(10))
